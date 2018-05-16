0845d15389cc926ec7d0c5731e3da2fb7bc6141e
aurorabora <borabora.choi@gmail.com>
2018-05-16 11:58:25 -0700

    added font-awesome link to index.html, finished components for  header, carousel, and show more button

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 ///front end index.js
 import '../node_modules/bootstrap3/dist/css/bootstrap.css';
 import '../node_modules/bootstrap3/dist/css/bootstrap-theme.css';
+// import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 import React from 'react';
 import ReactDOM from 'react-dom';
