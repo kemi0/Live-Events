6fe29f9cbc4d6347928e58bf2c946281add9b6d3
Katy Deng <deng2017.xq@gmail.com>
2018-05-16 20:28:08 -0700

    update back_end branch

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'; 
 import axios from 'axios';
-
+import helperList from '../../helper/helperList';
 class Test extends Component{
-  componentDidMount(){
-    this.getUserData();
-  }
+    componentDidMount(){
+      this.getUserData();
+    }
 
-  //requesting data from server: 
+  //requesting data from server by using '/api/user-data' : 
   async getUserData(){
     const response = await axios.get('/api/user-data');
-    console.log('user data response:', response.data);
+    console.log('user data response from database:', response.data);
+    <helperList/>
     // const name = response.data.name;
     // console.log(name);
   }
