c1e9f463f1dca7b5aefce145946ff5490cf27d05
Katy Deng <deng2017.xq@gmail.com>
2018-05-16 22:13:26 -0700

    fixed test.js import routing; ready to merge with front_end branch

--- client/helper/test.js
+++ client/helper/test.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'; 
 import axios from 'axios';
-import helperList from '../../helper/helperList';
+import helperList from './helperList';
 class Test extends Component{
     componentDidMount(){
       this.getUserData();
