c73347107b8d5d497abfe1e42267009082e39ea9
aurorabora <borabora.choi@gmail.com>
2018-05-20 22:02:11 -0700

    fixed bootstrap issue and everything is populating nicely for now

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 ///front end index.js
 import React from 'react';
+import '../node_modules/bootstrap3/dist/css/bootstrap.css';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import { BrowserRouter as Router } from 'react-router-dom';
