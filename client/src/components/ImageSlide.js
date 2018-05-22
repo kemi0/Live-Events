9d4ec3aeb7d52afe5d15df8a2584b99f85cc3d43
aurorabora <borabora.choi@gmail.com>
2018-05-22 15:56:23 -0700

    working on image slider

--- client/src/components/ImageSlide.js
+++ client/src/components/ImageSlide.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ImageSlide =({ url }) => {
-    const ImagineStyling ={
-        backgroundImage:`url(${url})`,
-        backgroundSize: 'cover',
-        backgroundPosition:'center'
-    };
 
-    return(
-        <div className="Iiage-slide" style={styles}></div>
-    );
+
+export default class ImageSlide extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state={
+            slideCount:1
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="slider">
+            </div>
+        );
+    }
 }
