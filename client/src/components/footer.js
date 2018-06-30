fa685c47cf48a7bab8c03e81f85dba9f8f071923
jeanpaul <jeanpaul.olukemi@gmail.com>
2018-06-29 18:06:18 -0700

    crafting the event details

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -4,21 +4,17 @@ import '../assets/css/footer.css';
 class Footer extends Component {
     render() {
     
-         return (
-            
-<footer className="flex-rw">
-            <ul className="footer-list-top">
-                
-            </ul>
-            <section className="footer-social-section flex-rw">
+         return (   
+            <footer className="flex-rw">
+                <section className="footer-social-section">
                     <span className="footer-social-overlap footer-social-icons-wrapper">
                         <a href="https://www.facebook.com/"  target="_blank" title="Facebook" itemProp="significantLink"><i className="generic-anchor fa fa-facebook"></i></a>
                         <a href="https://twitter.com/" target="_blank" title="Twitter" itemProp="significantLink"><i className="generic-anchor fa fa-twitter"></i></a>
                         <a href="http://instagram.com/"  target="_blank" title="Instagram" itemProp="significantLink"><i className="generic-anchor fa fa-instagram"></i></a>
                         <a href="https://www.youtube.com/"  target="_blank" title="Youtube" itemProp="significantLink"><i className=" generic-anchor fa fa-youtube"></i></a>
                     </span>
-            </section>
-</footer>
+                </section>
+            </footer>
                       
         );
     }
